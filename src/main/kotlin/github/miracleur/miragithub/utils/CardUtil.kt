c2b2ad26f264bd89a5c361b4a47c691fc7654995
圣迹 <miracleur1314@outlook.com>
2024-11-03 19:07:13 +0800

    new picture

--- src/main/kotlin/github/miracleur/miragithub/utils/CardUtil.kt
+++ src/main/kotlin/github/miracleur/miragithub/utils/CardUtil.kt
@@ -7,12 +7,17 @@ import net.mamoe.mirai.message.data.*
 import net.mamoe.mirai.utils.ExternalResource
 import net.mamoe.mirai.utils.ExternalResource.Companion.toExternalResource
 import net.mamoe.mirai.utils.ExternalResource.Companion.uploadAsImage
+import java.awt.AlphaComposite
 import java.awt.Color
 import java.awt.Font
 import java.awt.Graphics2D
+import java.awt.geom.RoundRectangle2D
 import java.awt.image.BufferedImage
+import java.awt.image.ConvolveOp
+import java.awt.image.Kernel
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.io.File
 import java.net.URL
 import javax.imageio.ImageIO
 
@@ -82,7 +87,7 @@ class CardUtil {
         g.drawImage(key, x, y + 150, 40, 40, null)
 
         g.color = Color.BLACK
-        g.drawString("检测到 {$name} 为 ${projects.split("/")[1]} 提交了代码", x + 70, y + 32)
+        g.drawString("检测到 $name 为 ${projects.split("/")[1]} 提交了代码", x + 70, y + 32)
         g.drawString(time, x + 70, y + 50 + 32)
         g.drawString(message, x + 70, y + 100 + 32)
         g.drawString(html, x + 70, y + 150 + 32)
@@ -99,5 +104,82 @@ class CardUtil {
         }
 
         return Image(imageId)
+    }
+
+    @Throws(Exception::class)
+    suspend fun getCard(html: String, message: String, projects: String, avatar: String, name: String, time: String, event: Contact): Image {
+        val avatarResource: ExternalResource = ImageUtil.Companion.getImage(avatar).toByteArray().toExternalResource()
+        withContext(Dispatchers.IO) {
+            avatarResource.close()
+        }
+
+        val cardWidth = 1200
+        val cardHeight = 600
+        val cardImage = BufferedImage(cardWidth, cardHeight, BufferedImage.TYPE_INT_RGB)
+        val g: Graphics2D = cardImage.createGraphics()
+
+        //背景
+        val backgroundImage = ImageIO.read(File("back.png"))
+        g.drawImage(backgroundImage, 0, 0, cardImage.width, cardImage.height, null)
+
+        // 创建高斯模糊操作
+        val radius = 15
+        val kernelSize = radius * 2 + 1
+        val kernel = FloatArray(kernelSize * kernelSize) { 1f / (kernelSize * kernelSize) } // 均匀模糊
+        val convolveOp = ConvolveOp(Kernel(kernelSize, kernelSize, kernel), ConvolveOp.EDGE_NO_OP, null)
+
+        // 应用模糊效果
+        val blurredImage = convolveOp.filter(backgroundImage, null)
+        g.drawImage(blurredImage, 0, 0, 1200, 700, null)
+        //头像
+        val avatarInputStream = ByteArrayInputStream(ImageUtil.Companion.getImage(avatar).toByteArray())
+        val avatarBufferedImage = ImageIO.read(avatarInputStream)
+        drawRoundedImageWithOpacity(g, avatarBufferedImage, cardWidth / 20.0, 280.0, cardWidth / 3, cardHeight / 3, 10, 0.0F)
+
+        //框框
+        drawBox(g, cardWidth / 20.0, 400.0, cardWidth / 3, cardHeight / 3, 10, 0.5F)
+        drawBox(g, 290.0, 100.0, 1159 - 290, 400, 10, 0.5F)
+
+        //字
+        g.font = Font("Microsoft YaHei", Font.PLAIN, 32)
+        g.color = Color.BLACK
+        g.drawString(name, cardWidth / 20 + 20, 420 + 32)
+
+        g.dispose()
+
+        val outputStream = ByteArrayOutputStream()
+        ImageIO.write(cardImage, "jpg", outputStream)
+
+        val cardResource: ExternalResource = outputStream.toByteArray().toExternalResource()
+        val imageId: String = cardResource.uploadAsImage(event).imageId
+        withContext(Dispatchers.IO) {
+            cardResource.close()
+        }
+
+        return Image(imageId)
+    }
+
+    fun drawRoundedImageWithOpacity(g: Graphics2D, image: BufferedImage, x: Double, y: Double, width: Int, height: Int, cornerRadius: Int, opacity: Float) {
+        val roundedRectangle = RoundRectangle2D.Double(x, y, width.toDouble(), height.toDouble(), cornerRadius.toDouble(), cornerRadius.toDouble())
+        g.clip(roundedRectangle)
+
+        val composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity)
+        g.composite = composite
+
+        g.drawImage(image, x.toInt(), y.toInt(), width, height, null)
+
+        g.clip = null
+    }
+
+    fun drawBox(g: Graphics2D, x: Double, y: Double, width: Int, height: Int, cornerRadius: Int, opacity: Float) {
+        val roundedRectangle = RoundRectangle2D.Double(x, y, width.toDouble(), height.toDouble(), cornerRadius.toDouble(), cornerRadius.toDouble())
+
+        val composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity)
+        g.composite = composite
+
+        g.color = Color.WHITE
+        g.fill(roundedRectangle)
+
+        g.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f)
     }
 }
